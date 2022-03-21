c9c08a8bc8eff82f002c765aceb99ebe7550f2db
Nenad Đorđević <93268425+NenadDj97@users.noreply.github.com>
2022-03-21 21:49:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+import { Gradient } from './Gradient.js'
+  
+// Create your instance
+const gradient = new Gradient()
+
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas')
+
+function fader() {
+    var b = $('#fading'),
+        wh = $(window).height(),
+        dt = $(document).scrollTop(),
+
+        blueView = wh - (b.offset().top - dt),
+        op;
+
+    if (blueView > 0) {
+        op = 1 - 1 / (wh + b.height()) * blueView;
+        if (op > 0)
+            b.css({opacity: op});
+    }
+}
+// Event on scroll
+$(document).bind('scroll', fader);
+
+/* FEEDBACK owl carousel script */
+
+$('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeOut:2000,
+    autoplayHoverPause:true,
+    nav:true,
+    navText: ['<i class="fas fa-angle-left" aria-hidden="true"></i>','<i class="fas fa-angle-right" aria-hidden="true"></i>'],
+    navContainer: '.carousel .custom-nav',
+    responsive: {
+        0:{
+            items: 1
+        },
+        900:{
+            items: 2
+        },
+        1500:{
+            items: 3
+        }
+    }
+});
+
+$("#n-about").click(function() {
+    $('.content').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+
+$("#n-works").click(function() {
+    $('.content').animate({
+        scrollTop: $("#works").offset().top
+    }, 2000);
+});
+
+$("#n-services").click(function() {
+    $('.content').animate({
+        scrollTop: $("#services").offset().top
+    }, 2000);
+});
+
+$("#n-reviews").click(function() {
+    $('.content').animate({
+        scrollTop: $("#reviews").offset().top
+    }, 2000);
+});
